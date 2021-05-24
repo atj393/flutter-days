e8047aa64990b9e70a8416663c68301fc5a34bea
Alexis Toby <alexistobyjr@gmail.com>
2021-05-24 22:41:40 +0200

    Chapter 43 - Flutter Layouts Challenge

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -1,5 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: SafeArea(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+                child: Center(child: Text('Container 1')),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                child: Center(child: Text('Container 2')),
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.yellow.withOpacity(0.5),
+                      offset: Offset(0, 100),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+                child: Center(child: Text('Container 2')),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
